57c793f576ccc87d0eb524e974309f784ff227b6
bhishma8 <ishan.baban@gmail.com>
2020-08-26 22:45:53 +0530

    minor changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose=require("mongoose");
-const date=require(__dirname+"/date.js");
+
 
 const app = express();
 
@@ -17,7 +17,21 @@ const itemsSchema={
     name:String
 };
 const Item=mongoose.model("Item",itemsSchema);
+const item1=new Item({name:"Welcome to your todolist"});
+const item2=new Item({name:"Hit the + button to add a new item"});
+const item3=new Item({name:"<-- Hit this to delete an item."}) ;
 
+const defaultItems={item1,item2,item3};
+Item.insertMany(defaultItems,function(err){
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log("Successfully stored in DB");
+    }
+})
 app.get("/", function(req, res){
    
    
